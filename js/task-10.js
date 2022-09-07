7b99c841e4efeda5a6c8c2db1be1d74377c6cdc6
MiroProger <sirmiroslavsuprun@gmail.com>
2022-09-07 13:42:04 +0300

    Completed task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+  input: document.querySelector('[type="number"]'),
+};
+
+ref.btnCreate.addEventListener("click", createBoxes);
+ref.btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = parseInt(ref.input.value);
+  let markup = "";
+  for (let i = 1; i <= amount; i++) {
+    markup += `<div style="display: block;width:${i * 10 + 20}px;height:${
+      i * 10 + 20
+    }px; background-color:${getRandomHexColor()};"></div>`;
+  }
+  ref.boxes.innerHTML = markup;
+}
+
+function destroyBoxes() {
+  ref.boxes.innerHTML = "";
+}
